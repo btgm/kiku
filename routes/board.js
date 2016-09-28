3aef71debebae51cc75064da4886f5dded7f2e02
George Mandis <george@snaptortoise.com>
2016-09-28 14:32:04 -0700

    streamlined ajax responses

--- routes/board.js
+++ routes/board.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var gameActions = require('../gameActions.js');
+var swig = require('swig');
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Nothing!');
+  var gameState = req.session.gameState;
+  
+  var templates = {
+    'hand': swig.compileFile('views/templates/hand.html'),
+    'played': swig.compileFile('views/templates/played.html'),
+    'status': swig.compileFile('views/templates/status.html'),
+  }
+  
+  // return the HTML snippets using element IDs as key
+  var output = {
+    'hand-computer': templates['hand']( { hand: gameState['computer'], handKey: 'computer' } ),
+    'hand-player': templates['hand']( { hand: gameState['player'], handKey: 'player' } ),
+    'played-cards': templates['played']( { played: gameState.played } ),
+    'game-status': templates['status']( { gameState: gameState } ),
+  };
+  
+  res.set({
+    'Content-Type' : 'text/plain'
+  });
+    
+  res.send(output);
 });
 
 router.get('/hand/:handKey', function(req, res, next) {
